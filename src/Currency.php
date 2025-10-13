a07e00ff156aef10e824ce99022d5e8a3be99a1f
Benjamin Bruska <benjamin.bruska@fortuneglobe.com>
2025-10-14 00:32:28 +0200

    Add generic currency class

---
+++ src/Currency.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace ComponoKit\Money\Currencies;
+
+use ComponoKit\Money\Currencies\Exceptions\InvalidCurrencyException;
+use ComponoKit\Money\Interfaces\RepresentsCurrency;
+
+class Currency implements RepresentsCurrency
+{
+	private string $isoCode;
+
+	public function __construct( string $isoCode, private string $symbol, private int $minorUnitFactor )
+	{
+		if ( !preg_match( '/^[A-Z]{3}$/', strtoupper( $isoCode ) ) )
+		{
+			throw new InvalidCurrencyException( 'ISO code must be exactly 3 letters (A–Z)' );
+		}
+
+		if ( $this->minorUnitFactor <= 0 )
+		{
+			throw new InvalidCurrencyException( 'Minor unit factor must be greater than 0' );
+		}
+
+		$this->isoCode = strtoupper( $isoCode );
+	}
+
+	public function getIsoCode(): string
+	{
+		return $this->isoCode;
+	}
+
+	public function getSymbol(): string
+	{
+		return $this->symbol;
+	}
+
+	public function getMinorUnitFactor(): int
+	{
+		return $this->minorUnitFactor;
+	}
+
+	public function equals( RepresentsCurrency $other ): bool
+	{
+		return $this->isoCode === $other->isoCode;
+	}
+}
---
+++ src/Exceptions/InvalidCurrencyException.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace ComponoKit\Money\Currencies\Exceptions;
+
+class InvalidCurrencyException extends \LogicException
+{
+
+}
